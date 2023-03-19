ecba4b0eb846d004d81e894f349ebfede269c99a
Halyna Tyzhbir <galynatyzhbir@gmail.com>
2023-03-19 02:25:06 +0200

    message

--- src/index.js
+++ src/index.js
@@ -14,19 +14,24 @@ let currentPage = 1;
 const getPictures = async () => {
     try {
         const KEY = "34507667-10e09daff0142ff80b56bfc1b";
-        const QUERY = refs.searchQuery.value;
+        const QUERY = refs.searchQuery.value.trim();
+        
         const Response = await axios.get
             (`https://pixabay.com/api/?key=${KEY}&q=${QUERY}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=40`);
+    
         if (Response.data.hits.length === 0 || QUERY === "") {
             Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
         } else {
             insertContent(Response.data.hits);
-            currentPage += 1;
-            refs.loadMoreBtn.classList.remove("hide");
-            if (Response.data.hits.length < 40) {
+            if (Response.data.hits.length === 40 && currentPage === 1) {
+                refs.loadMoreBtn.classList.remove("hide");
+            } else if (Response.data.hits.length < 40 && currentPage !== 1) {
                 refs.loadMoreBtn.classList.add("hide");
                 Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
-            }
+            };
+            if (Response.data.hits.length !== 0 && currentPage === 1) {
+                Notiflix.Notify.success(`Hooray! We found ${Response.data.totalHits} images.`);
+            }; 
         }
     }
     catch (error) {
@@ -38,8 +43,12 @@ const handleSubmit = (e) => {
     e.preventDefault();
     if (e.type === "submit") {
         refs.gallery.innerHTML = "";
+        currentPage = 1;
         refs.loadMoreBtn.classList.add("hide");
     };
+    if (e.type === "click") {
+        currentPage += 1;
+    }
     getPictures();
 }
 
